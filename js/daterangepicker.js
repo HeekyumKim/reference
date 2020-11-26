024e796eb5f4c3fcc800a40255f54c411aca9fdb
kyoungsun <sunlove1982@neonexsoft.com>
2020-11-26 16:55:21 +0900

    등록 수정 및 상세신규

--- js/daterangepicker.js
+++ js/daterangepicker.js
@@ -704,7 +704,7 @@
                 html += '<th></th>';
             }
 
-            var dateHtml = this.locale.monthNames[calendar[1][1].month()] + calendar[1][1].format(" YYYY");
+            var dateHtml = calendar[1][1].format(" YYYY")+"년 " + this.locale.monthNames[calendar[1][1].month()] ;
 
             if (this.showDropdowns) {
                 var currentMonth = calendar[1][1].month();
@@ -736,7 +736,7 @@
                 }
                 yearHtml += '</select>';
 
-                dateHtml = monthHtml + yearHtml;
+                dateHtml =  yearHtml + monthHtml ;
             }
 
             html += '<th colspan="5" class="month">' + dateHtml + '</th>';
